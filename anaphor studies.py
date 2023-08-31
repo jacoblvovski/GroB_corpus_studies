d5258e83e1507de28d6fe8214db040777da68f39
jacoblvovski <105866849+jacoblvovski@users.noreply.github.com>
2023-08-31 11:07:44 +0300

    Add files via upload

---
+++ anaphor studies.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+from temporarymodules.simplecompling import *
+
+poems_df = pd.read_csv('Корпус Летова v1.1_NER.csv', encoding='utf-8')
+
+poems = list(poems_df['text'])
+poems = [str(poem) for poem in poems]
+dicts_with_shares = [calculate_share_of_repeated_elements(poem) for poem in poems]
+lines_df = pd.DataFrame.from_records(dicts_with_shares)
+
+poems_df_new = pd.concat([poems_df, lines_df])
+
+lines_df.to_excel('Метрики анафоры.xlsx')
